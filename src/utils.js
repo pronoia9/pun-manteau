3b8f2e739bef1ab6f40a46921998d7efdee332ff
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-13 19:22:38 +0300

    add some theme related utils

---
+++ src/utils.js
@@ -0,0 +1,15 @@
+import { themes } from "styles";
+
+// GENETAL UTILS
+export const lowerCase = (t) => `${t}`.toLowerCase();
+
+export const rem = (val) => `${parseInt(val) / 16}rem`;
+
+// THEME RELATED
+export const getSystemTheme = () => (window?.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+
+export const getTheme = (theme) => (theme === 'light' ? themes.light : themes.dark);
+
+export const isDarkTheme = (theme) => theme === 'dark';
+
+export const systemThemeChangeHandler = (e, setTheme) => { setTheme(e.matches ? 'dark' : 'light'); };
