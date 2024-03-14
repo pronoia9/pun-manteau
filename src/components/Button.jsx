e8a253cb73f9c1e9bcc87cd958922d7a8b8bbd53
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-15 02:06:09 +0300

    init button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,47 @@
-export const Button = () => {
-  return <div>Button</div>;
+import styled from 'styled-components';
+
+import { rem } from './utils';
+
+export const Button = ({ showOverlay, setShowOverlay }) => {
+  const handleClick = () => {
+    setShowOverlay((prev) => !prev);
+  };
+
+  return (
+    <Container onClick={handleClick}>
+      {!showOverlay ? 'MORE' : 'LESS'}
+      <img src='/icons/icon-arrow-up.svg' />
+    </Container>
+  );
 };
+
+const Container = styled.button`
+  padding: ${rem(8)} ${rem(9)};
+  position: relative;
+  color: rgba(0, 0, 0, 1);
+  font-size: 1rem;
+  font-weight: bold;
+  letter-spacing: ${rem(5)};
+  line-height: ${rem(28)};
+  fill: rgb(255, 255, 255);
+  background: rgb(255, 255, 255);
+  border-radius: ${rem(28)};
+
+  img {
+    content: '';
+    width: ${rem(40)};
+    height: ${rem(40)};
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    background: var(--c-secondary);
+    border-radius: 50%;
+  }
+
+  &:hover {
+    img {
+      background-color: rgb(153, 153, 153);
+    }
+  }
+`;
