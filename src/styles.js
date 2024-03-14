8d19fe77b7bb8bb82c9d2172dac302e43ea77dcb
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-14 23:30:42 +0300

    add heading styles

--- src/styles.js
+++ src/styles.js
@@ -77,27 +77,50 @@ export const GlobalStyles = createGlobalStyle`
     background-size: cover;
   }
 
-  ${'' /* H1; Inter Bold; 200px; 200px Line; -5 Spacing */}
+  /* H1; Inter Bold; 200px; 200px Line; -5 Spacing */
   h1 {
+    font-weight: bold;
+    font-size: ${rem(200)};
+    line-height: ${rem(200)};
+    letter-spacing: ${rem(-5)};
   }
 
-  ${'' /* H2; Inter Bold; 56px; 68px Line */}
+  /* H2; Inter Bold; 56px; 68px Line */
   h2 {
+    font-weight: bold;
+    font-size: ${rem(56)};
+    line-height: ${rem(68)};
   }
 
-  ${'' /* H3; Inter Bold; 24px; 28px Line; -4,8 Spacing; All Caps */}
+  /* H3; Inter Bold; 24px; 28px Line; -4,8 Spacing; All Caps */
   h3 {
+    font-weight: bold;
+    font-size: ${rem(24)};
+    line-height: ${rem(28)};
+    letter-spacing: ${rem(-4.8)};
+    text-transform: uppercase;
   }
 
-  ${'' /* H4; Inter Regular; 20px; 28px Line; +4 Spacing */}
+  /* H4; Inter Regular; 20px; 28px Line; +4 Spacing */
   h4 {
+    font-weight: normal;
+    font-size: ${rem(20)};
+    line-height: ${rem(28)};
+    letter-spacing: ${rem(4)};
   }
 
-  ${'' /* H5; Inter Bold; 18px; 28px Line */}
+  /* H5; Inter Bold; 18px; 28px Line */
   h5 {
+    font-weight: bold;
+    font-size: ${rem(18)};
+    line-height: ${rem(28)};
   }
 
-  ${'' /* H6; Inter Bold; 15px; 28px Line; +3 Spacing */}
+  /* H6; Inter Bold; 15px; 28px Line; +3 Spacing */
   h6 {
+    font-weight: bold;
+    font-size: ${rem(15)};
+    line-height: ${rem(28)};
+    letter-spacing: ${rem(3)};
   }
 `;
