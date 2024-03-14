54cfdd9ee08c4f4c5d8d8cdd452129c44b254b84
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-15 01:58:32 +0300

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import { ThemeProvider } from 'styled-components';
+import { useState } from 'react';
+import styled, { ThemeProvider } from 'styled-components';
 
+import { Quote, Time, Button, Overlay } from './components';
 import { GlobalStyles } from './styles';
 import { getTheme } from './utils';
 
 function App() {
+  const [showOverlay, setShowOverlay] = useState(false);
+
   return (
     <ThemeProvider theme={getTheme('dark')}>
       <GlobalStyles />
+      <Container>
+        <Quote />
+        <Time />
+        <Button setShowOverlay={setShowOverlay} />
+        {showOverlay && <Overlay />}
+      </Container>
     </ThemeProvider>
   );
 }
 
 export default App;
+
+const Container = styled.div``;
