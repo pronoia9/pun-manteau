80459e715e66fb481a300fb10a37d1ef9c32ec87
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-15 02:48:43 +0300

    format + fix letter spacing

--- src/styles.js
+++ src/styles.js
@@ -3,7 +3,6 @@ import { createGlobalStyle } from 'styled-components';
 import { themes, rem } from './utils';
 
 export const GlobalStyles = createGlobalStyle`
-  ${'' /* @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap'); */}
   @font-face {
     font-family: Inter;
     font-style: normal;
@@ -19,14 +18,8 @@ export const GlobalStyles = createGlobalStyle`
     --font: Inter;
     --font-fallback: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     --image: url(${({ theme }) => theme.image.desktop});
-
-    @media (max-width: 768px) {
-      --image: url(${({ theme }) => theme.image.tablet});
-    }
-
-    @media (max-width: 480px) {
-      --image: url(${({ theme }) => theme.image.mobile});
-    }
+    @media (max-width: 768px) { --image: url(${({ theme }) => theme.image.tablet}); }
+    @media (max-width: 480px) { --image: url(${({ theme }) => theme.image.mobile}); }
   }
 
   body {
@@ -39,8 +32,7 @@ export const GlobalStyles = createGlobalStyle`
       margin: 0;
       overflow: hidden;
     }
-
-    ${'' /* Body; Inter Regular; 18px; 28px line */}
+    
     font-family: var(--font) var(--font-fallback);
     font-size: ${rem(18)};
     color: rgb(255, 255, 255);
@@ -65,50 +57,44 @@ export const GlobalStyles = createGlobalStyle`
     }
   }
 
-  /* H1; Inter Bold; 200px; 200px Line; -5 Spacing */
   h1 {
     font-weight: bold;
     font-size: ${rem(200)};
     line-height: ${rem(200)};
-    letter-spacing: ${rem(-5)};
+    letter-spacing: -5;
   }
 
-  /* H2; Inter Bold; 56px; 68px Line */
   h2 {
     font-weight: bold;
     font-size: ${rem(56)};
     line-height: ${rem(68)};
   }
 
-  /* H3; Inter Bold; 24px; 28px Line; -4,8 Spacing; All Caps */
   h3 {
     font-weight: bold;
     font-size: ${rem(24)};
     line-height: ${rem(28)};
-    letter-spacing: ${rem(-4.8)};
+    letter-spacing: -4.8;
     text-transform: uppercase;
   }
 
-  /* H4; Inter Regular; 20px; 28px Line; +4 Spacing */
   h4 {
     font-weight: normal;
     font-size: ${rem(20)};
     line-height: ${rem(28)};
-    letter-spacing: ${rem(4)};
+    letter-spacing: 4;
   }
 
-  /* H5; Inter Bold; 18px; 28px Line */
   h5 {
     font-weight: bold;
     font-size: ${rem(18)};
     line-height: ${rem(28)};
   }
 
-  /* H6; Inter Bold; 15px; 28px Line; +3 Spacing */
   h6 {
     font-weight: bold;
     font-size: ${rem(15)};
     line-height: ${rem(28)};
-    letter-spacing: ${rem(3)};
+    letter-spacing: 3;
   }
 `;
