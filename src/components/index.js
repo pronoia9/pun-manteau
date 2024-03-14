f6fc3af334e058a43c2077661198a2da93d820c4
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-15 01:56:33 +0300

    init components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import { Button } from './Button';
+import { Overlay } from './Overlay';
+import { Quote } from './Quote';
+import { Time } from './Time';
+
+export { Button, Overlay, Quote, Time };
