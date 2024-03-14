e08a4941f088f212b0505b18c69d297c78ec30c3
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-15 02:51:43 +0300

    init temporary quote content

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,3 +1,18 @@
+import styled from 'styled-components';
+
 export const Quote = () => {
-  return <div>Quote</div>;
+  return (
+    <Container>
+      <Text>
+        “The science of operations, as derived from mathematics more especially, is a science of itself, and has its own abstract truth and value.”
+      </Text>
+      <Author>Ada Lovelace</Author>
+    </Container>
+  );
 };
+
+const Container = styled.div``;
+
+const Text = styled.div``;
+
+const Author = styled.h6``;
