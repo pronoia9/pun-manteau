fd70e84c2b1918e70d738b11b4d4c37019c19430
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-15 02:56:50 +0300

    init time content with filler

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,3 +1,32 @@
+import styled from 'styled-components';
+
 export const Time = () => {
-  return <div>Time</div>;
+  return (
+    <Container>
+      <Text>
+        <img src='/icons/icon-moon.svg' />
+        <p>Good Evening, It&apos;s Currently</p>
+      </Text>
+      
+      <Clock>
+        <h1>23:14</h1> <span>BST</span>
+      </Clock>
+
+      <Location>In London, UK</Location>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Text = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const Clock = styled.div``;
+
+const Location = styled.h3``;
