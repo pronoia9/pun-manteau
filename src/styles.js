007388af7d19fd559dfe53bfb825f8c06eafe65f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-14 21:23:18 +0300

    format

--- src/styles.js
+++ src/styles.js
@@ -14,40 +14,54 @@ export const themes = {
 };
 
 export const GlobalStyles = createGlobalStyle`
-${'' /* @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap'); */}
-@font-face {
-  font-family: Inter;
-  font-style: normal;
-  font-weight: 100 900;
-  font-display: swap;
-  src: url('./assets/font/Inter-VariableFont_slnt,wght.ttf') format('truetype');
-}
+  ${'' /* @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap'); */}
+  @font-face {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 100 900;
+    font-display: swap;
+    src: url('./assets/font/Inter-VariableFont_slnt,wght.ttf') format('truetype');
+  }
 
-:root {
-  --c-font: ${({ theme }) => theme.font.rgb};
-  --c-background: ${({ theme }) => theme.background.rgb};
-  --c-secondary: ${({ theme }) => theme.secondary.rgb};
-  --font: Inter;
-  --font-fallback: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-}
+  :root {
+    --c-font: ${({ theme }) => theme.font.rgb};
+    --c-background: ${({ theme }) => theme.background.rgb};
+    --c-secondary: ${({ theme }) => theme.secondary.rgb};
+    --font: Inter;
+    --font-fallback: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
 
-*::-webkit-scrollbar {
-  display: none;
-  -ms-overflow-style: none;
-  scrollbar-width: none;
-}
+  *::-webkit-scrollbar {
+    display: none;
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+  }
 
-${'' /* Body; Inter Regular; 18px; 28px line */}
+  ${'' /* Body; Inter Regular; 18px; 28px line */}
+  body {
+  }
 
-${'' /* H1; Inter Bold; 200px; 200px Line; -5 Spacing */}
+  ${'' /* H1; Inter Bold; 200px; 200px Line; -5 Spacing */}
+  h1 {
+  }
 
-${'' /* H2; Inter Bold; 56px; 68px Line */}
+  ${'' /* H2; Inter Bold; 56px; 68px Line */}
+  h2 {
+  }
 
-${'' /* H3; Inter Bold; 24px; 28px Line; -4,8 Spacing; All Caps */}
+  ${'' /* H3; Inter Bold; 24px; 28px Line; -4,8 Spacing; All Caps */}
+  h3 {
+  }
 
-${'' /* H4; Inter Regular; 20px; 28px Line; +4 Spacing */}
+  ${'' /* H4; Inter Regular; 20px; 28px Line; +4 Spacing */}
+  h4 {
+  }
 
-${'' /* H5; Inter Bold; 18px; 28px Line */}
+  ${'' /* H5; Inter Bold; 18px; 28px Line */}
+  h5 {
+  }
 
-${'' /* H6; Inter Bold; 15px; 28px Line; +3 Spacing */}
+  ${'' /* H6; Inter Bold; 15px; 28px Line; +3 Spacing */}
+  h6 {
+  }
 `;
