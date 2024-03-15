0b7d0f391fbefc72ae85b69a76024b826e263ac7
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-15 06:29:05 +0300

    add styles

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,15 +1,20 @@
 import styled from 'styled-components';
 
-export const Time = () => {
+import { getTimeHours, getTimeMinutes, getTimeSeconds, isNightTime, rem } from '../utils';
+
+export const Time = ({ time }) => {
   return (
     <Container>
       <Text>
-        <img src='/icons/icon-moon.svg' />
-        <p>Good Evening, It&apos;s Currently</p>
+        <img src={`/icons/icon-${isNightTime(time) ? 'moon' : 'sun'}.svg`} />
+        <h4>Good {isNightTime(time) ? 'evening' : 'morning'}, it&apos;s currently</h4>
       </Text>
-      
+
       <Clock>
-        <h1>23:14</h1> <span>BST</span>
+        <h1 key={time}>
+          {getTimeHours(time) || '23'}:{getTimeMinutes(time) || '14'}
+        </h1>{' '}
+        <h5>BST</h5>
       </Clock>
 
       <Location>In London, UK</Location>
@@ -25,8 +30,25 @@ const Container = styled.div`
 const Text = styled.div`
   display: flex;
   flex-direction: row;
+  gap: ${rem(17.63)};
+  align-items: center;
+  text-transform: uppercase;
+
+  img {
+    width: auto;
+    height: ${rem(24)};
+  }
 `;
 
-const Clock = styled.div``;
+const Clock = styled.div`
+  h1, h5 {
+    display: inline-block;
+  }
+
+  h5 {
+    font-size: ${rem(40)};
+    font-weight: lighter;
+  }
+`;
 
 const Location = styled.h3``;
