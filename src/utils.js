80ba29f467e3b1f9fcf3098ae930de8d43aac7c0
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-15 21:31:43 +0300

    update theme functions

--- src/utils.js
+++ src/utils.js
@@ -5,38 +5,6 @@ export const lowerCase = (t) => `${t}`.toLowerCase();
 
 export const rem = (val) => `${parseInt(val) / 16}rem`;
 
-// THEME RELATED
-export const themes = {
-  light: {
-    font: { hex: '#000000', rgb: 'rgb(0,0,0)', hsl: 'hsl(0, 0%, 0%)' },
-    background: { hex: '#FFFFFF', rgb: 'rgb(255, 255, 255)', hsl: 'hsl(0, 0%, 100%)' },
-    image: {
-      desktop: '/images/bg-desktop-image-daytime.jpg',
-      tablet: '/images/bg-tablet-image-daytime.jpg',
-      mobile: '/images/bg-mobile-image-daytime.jpg',
-    },
-  },
-  dark: {
-    font: { hex: '#FFFFFF', rgb: 'rgb(255, 255, 255)', hsl: 'hsl(0, 0%, 100%)' },
-    background: { hex: '#000000', rgb: 'rgb(0,0,0)', hsl: 'hsl(0, 0%, 0%)' },
-    image: {
-      desktop: '/images/bg-desktop-image-nighttime.jpg',
-      tablet: '/images/bg-tablet-image-nighttime.jpg',
-      mobile: '/images/bg-mobile-image-nighttime.jpg',
-    },
-  },
-};
-
-export const getSystemTheme = () => (window?.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
-
-export const getTheme = (theme) => (theme === 'light' ? themes.light : themes.dark);
-
-export const isDarkTheme = (theme) => theme === 'dark';
-
-export const systemThemeChangeHandler = (e, setTheme) => {
-  setTheme(e.matches ? 'dark' : 'light');
-};
-
 // QUOTE
 export const defaultQuote = {
   id: 'default',
@@ -107,3 +75,27 @@ export const getTimeOfDayString = (time) => {
   if (hour >= 18 && hour < 5) return 'afternoon';
   return 'ERR';
 };
+
+// THEME RELATED
+export const themes = {
+  light: {
+    font: { hex: '#000000', rgb: 'rgb(0,0,0)', hsl: 'hsl(0, 0%, 0%)' },
+    background: { hex: '#FFFFFF', rgb: 'rgb(255, 255, 255)', hsl: 'hsl(0, 0%, 100%)' },
+    image: {
+      desktop: '/images/bg-desktop-image-daytime.jpg',
+      tablet: '/images/bg-tablet-image-daytime.jpg',
+      mobile: '/images/bg-mobile-image-daytime.jpg',
+    },
+  },
+  dark: {
+    font: { hex: '#FFFFFF', rgb: 'rgb(255, 255, 255)', hsl: 'hsl(0, 0%, 100%)' },
+    background: { hex: '#000000', rgb: 'rgb(0,0,0)', hsl: 'hsl(0, 0%, 0%)' },
+    image: {
+      desktop: '/images/bg-desktop-image-nighttime.jpg',
+      tablet: '/images/bg-tablet-image-nighttime.jpg',
+      mobile: '/images/bg-mobile-image-nighttime.jpg',
+    },
+  },
+};
+
+export const getTheme = (time) => themes[isNightTime(time) ? 'dark' : 'light'];
