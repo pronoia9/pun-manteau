427735d5445ed549a35aa7fd8c6a1fbe2c3bdf18
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-15 21:12:51 +0300

    add h5 + more styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ import { rem } from '../utils';
 export const Button = ({ showOverlay, setShowOverlay }) => {
   return (
     <Container onClick={() => void setShowOverlay((prev) => !prev)} $showOverlay={showOverlay}>
-      {!showOverlay ? 'MORE' : 'LESS'}
+      <h5>{!showOverlay ? 'MORE' : 'LESS'}</h5>
       <img src='/icons/icon-arrow-up.svg' />
     </Container>
   );
@@ -20,25 +20,27 @@ const Container = styled.button`
   gap: ${rem(13)};
   align-items: center;
 
-  color: rgba(0, 0, 0, 0.5);
-  font-size: 1rem;
-  font-weight: bold;
-  letter-spacing: ${rem(5)};
-  line-height: ${rem(28)};
-
   background: rgb(255, 255, 255);
   border-radius: ${rem(28)};
+
+  h5 {
+    color: #000;
+    font-size: 1rem;
+    letter-spacing: ${rem(5)};
+    opacity: 0.5;
+  }
 
   img {
     width: ${rem(40)};
     height: ${rem(40)};
     border-radius: 50%;
-    transform: rotate(${({$showOverlay}) => $showOverlay ? '0' : '180'}deg);
+    opacity: ${({ $showOverlay }) => ($showOverlay ? '0.5' : '1')};
+    transform: rotate(${({ $showOverlay }) => ($showOverlay ? '0' : '180')}deg);
   }
 
   &:hover {
     img {
-      background-color: rgb(153, 153, 153);
+      opacity: 0.5;
     }
   }
 `;
