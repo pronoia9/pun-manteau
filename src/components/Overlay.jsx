7fe9b5bd0cc2325f7c2790ce342ac5653dbcee67
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-17 06:33:56 +0300

    use before and after for the bg blur

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -23,18 +23,39 @@ export const Overlay = ({ time }) => {
         <Group title='Day of the Week' subtitle={time.day_of_week} />
         <Group title='Week Number' subtitle={time.week_number} />
       </Right>
-
-      <Background />
-      <Blur />
     </Container>
   );
 };
 
 const Container = styled.div`
+  height: 400px;
   display: flex;
   flex-direction: row;
   color: var(--c-font);
   position: relative;
+
+  &:before,
+  &:after {
+    content: '';
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+  }
+
+  &:before {
+    background-color: var(--c-background);
+    opacity: 0.75;
+    z-index: -2;
+  }
+
+  &:after {
+    backdrop-filter: blur(10px);
+    z-index: -1;
+  }
 `;
 
 const GroupContainer = styled.div`
@@ -47,23 +68,3 @@ const Left = styled.div``;
 const Right = styled(Left)``;
 
 const HR = styled.div``;
-
-const OverlayBackgroundBlur = styled.div`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  z-index: -1;
-`;
-
-const Background = styled(OverlayBackgroundBlur)`
-  background-color: var(--c-background);
-  opacity: 0.75;
-`;
-
-const Blur = styled(OverlayBackgroundBlur)`
-  backdrop-filter: blur(40.77px);
-`;
