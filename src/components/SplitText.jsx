159cb060703c7c034a3b946a4e38d66a9b5d1ecb
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-21 01:18:01 +0300

    revert back to separate text & child

--- src/components/SplitText.jsx
+++ src/components/SplitText.jsx
@@ -2,21 +2,29 @@ import { motion } from 'framer-motion';
 
 import { textType } from '../motion';
 
-export const SplitText = ({ text, elementType = 'span', speed, br = null, childrenProps = {}, children, ...props }) => {
+export const SplitText = ({ text, elementType = 'span', speed, br = null, charProps, childrenProps = {}, children, ...props }) => {
   const Element = motion[elementType]; // Dynamically select the appropriate motion element
 
   return (
     <Element className='split-text' {...textType.text(speed)} {...props}>
-      {(text || typeof children === 'string') &&
-        `${text || children}`.split('').map((c, index) => (
-          <motion.span key={`split-text-${children}-${index}-${c}`} {...textType.char} {...childrenProps}>
+      {text &&
+        `${text}`.split('').map((c, index) => (
+          <motion.span key={`split-text-${children}-${index}-${c}`} {...textType.char} {...charProps}>
             {c}
           </motion.span>
         ))}
 
       {br && <br />}
 
-      {children}
+      {typeof children === 'string' ? (
+        `${children}`.split('').map((c, index) => (
+          <motion.span key={`split-text-${children}-${index}-${c}`} {...textType.char} {...childrenProps}>
+            {c}
+          </motion.span>
+        ))
+      ) : (
+        <>{children}</>
+      )}
     </Element>
   );
 };
