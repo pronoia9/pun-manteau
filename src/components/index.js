f5782c9f045945e9543d46b07342e387bbe62edb
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-20 22:42:03 +0300

    add split text

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import { Button } from './Button';
 import { Overlay } from './Overlay';
 import { Quote } from './Quote';
+import { SplitText } from './SplitText';
 import { Time } from './Time';
 
-export { Button, Overlay, Quote, Time };
+export { Button, Overlay, Quote, SplitText, Time };
