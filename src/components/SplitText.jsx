e238fee720bbe6f5817da253ff80dc0cbf0c8fd2
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-21 00:47:04 +0300

    remove text/charOptions

--- src/components/SplitText.jsx
+++ src/components/SplitText.jsx
@@ -2,23 +2,23 @@ import { motion } from 'framer-motion';
 
 import { textType } from '../motion';
 
-export const SplitText = ({ text, elementType = 'span', speed, textOptions = {}, charOptions = {}, br = null, children, childrenProps, ...props }) => {
+export const SplitText = ({ text, elementType = 'span', speed, br = null, childrenProps = {}, children, ...props }) => {
   const Element = motion[elementType]; // Dynamically select the appropriate motion element
 
   return text || children ? (
-    <Element className='split-text' {...textType.text(speed)} {...textOptions} {...props}>
+    <Element className='split-text' {...textType.text(speed)} {...props}>
       {text &&
         `${text}`.split('').map((c, index) => (
-          <motion.span key={`split-text-${children}-${index}-${c}`} {...textType.char} {...charOptions}>
+          <motion.span key={`split-text-${children}-${index}-${c}`} {...textType.char} {...childrenProps}>
             {c}
           </motion.span>
         ))}
 
       {br && <br />}
-      
+
       {children && typeof children === 'string' ? (
         `${children}`.split('').map((c, index) => (
-          <motion.span key={`split-text-${children}-${index}-${c}`} {...textType.char} {...charOptions} {...childrenProps}>
+          <motion.span key={`split-text-${children}-${index}-${c}`} {...textType.char} {...childrenProps}>
             {c}
           </motion.span>
         ))
