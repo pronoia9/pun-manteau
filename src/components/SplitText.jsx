1621eee32257adaa4d3dda90e14af5961e88a56b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-21 00:04:05 +0300

    add br prop

--- src/components/SplitText.jsx
+++ src/components/SplitText.jsx
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion';
 
 import { textType } from '../motion';
 
-export const SplitText = ({ text, elementType = 'span', speed, textOptions = {}, charOptions = {}, children, childrenProps, ...props }) => {
+export const SplitText = ({ text, elementType = 'span', speed, textOptions = {}, charOptions = {}, br = null, children, childrenProps, ...props }) => {
   const Element = motion[elementType]; // Dynamically select the appropriate motion element
 
   return text || children ? (
@@ -14,6 +14,8 @@ export const SplitText = ({ text, elementType = 'span', speed, textOptions = {},
           </motion.span>
         ))}
 
+      {br && <br />}
+      
       {children && typeof children === 'string' ? (
         `${children}`.split('').map((c, index) => (
           <motion.span key={`split-text-${children}-${index}-${c}`} {...textType.char} {...charOptions} {...childrenProps}>
