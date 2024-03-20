22f0a889c81aee8c16168f70060ff7f850f73eb7
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-20 23:04:20 +0300

    update for text & children and main and children props

--- src/components/SplitText.jsx
+++ src/components/SplitText.jsx
@@ -3,16 +3,24 @@ import PropTypes from 'prop-types';
 
 import { textType } from '../motion';
 
-export const SplitText = ({ text, elementType = 'span', speed, textOptions = {}, charOptions = {}, children, ...props }) => {
+export const SplitText = ({ text, elementType = 'span', speed, textOptions = {}, charOptions = {}, children, childrenProps, ...props }) => {
   const Element = motion[elementType]; // Dynamically select the appropriate motion element
 
   return text || children ? (
     <Element className='split-text' {...textType.text(speed)} {...textOptions} {...props}>
-      {`${text ? text : children}`.split('').map((c, index) => (
-        <motion.span key={`split-text-${children}-${index}-${c}`} {...textType.char} {...charOptions}>
-          {c}
-        </motion.span>
-      ))}
+      {text &&
+        `${text}`.split('').map((c, index) => (
+          <motion.span key={`split-text-${children}-${index}-${c}`} {...textType.char} {...charOptions}>
+            {c}
+          </motion.span>
+        ))}
+
+      {children &&
+        `${children}`.split('').map((c, index) => (
+          <motion.span key={`split-text-${children}-${index}-${c}`} {...textType.char} {...charOptions} {...childrenProps}>
+            {c}
+          </motion.span>
+        ))}
     </Element>
   ) : (
     <></>
