0de62621825c3739cecfcfca4ce07552d341f7ae
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-20 21:14:39 +0300

    init time motion props

--- src/motion.js
+++ src/motion.js
@@ -1,13 +1,59 @@
 //--- QUOTE
-export const quoteMotion = {};
+export const quoteMotion = {
+  container: { variants: {} },
+
+  quote: { variants: {} },
+
+  author: { variants: {} },
+
+  button: (initial = 'hidden') => ({
+    initial,
+    whileHover: 'hover',
+    whileTap: 'tap',
+    variants: {
+      hidden: { scale: 0, opacity: 0 },
+      show: { scale: 1, opacity: 1, transition: { type: 'spring', bounce: 0.5 } },
+      hover: {
+        scale: 1.2,
+        filter: 'invert(20%) sepia(243%) saturate(1576%) hue-rotate(-21deg) brightness(137%) contrast(73%)',
+        rotate: [0, 0, 180, 360],
+        transition: { rotate: { type: 'tween', delay: 5, duration: 1, repeat: Infinity, repeatDelay: 5 } },
+      },
+      tap: {
+        scale: 0.9,
+        filter: 'invert(20%) sepia(243%) saturate(1576%) hue-rotate(-21deg) brightness(137%) contrast(73%)',
+        rotate: [0, 0, 180, 360],
+        transition: { type: 'tween', duration: 0.75 },
+      },
+      move: {
+        rotate: [0, 0, 180, 360],
+        transition: { type: 'spring' },
+      },
+    },
+  }),
+};
 
 //--- TIME
-export const timeMotion = {};
+export const timeMotion = {
+  container: {},
+
+  icon: {},
+
+  text: {},
+
+  time: {},
+
+  blink: {},
+  
+  zone: {},
+
+  location: {}
+};
 
 //--- BUTTON
 export const buttonMotion = {
   container: { variants: {} },
-  
+
   text: { variants: {} },
 
   image: { variants: {} },
