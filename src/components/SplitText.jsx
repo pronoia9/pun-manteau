4af94373ff8231686059498cc78864c47a28b20e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-20 22:48:21 +0300

    add text || children

--- src/components/SplitText.jsx
+++ src/components/SplitText.jsx
@@ -2,11 +2,11 @@ import { motion } from 'framer-motion';
 
 import { textType } from '../motion';
 
-export const SplitText = ({ speed, text = {}, char = {}, children }) => {
-  return children ? (
-    <motion.span className='split-text' {...textType.text(speed)} {...text}>
-      {`${children}`.split('').map((c, index) => (
-        <motion.span key={`split-text-${children}-${index}-${c}`} {...textType.char} {...char}>
+export const SplitText = ({ text, speed, textOptions = {}, charOptions = {}, children }) => {
+  return text || children ? (
+    <motion.span className='split-text' {...textType.text(speed)} {...textOptions}>
+      {`${text ? text : children}`.split('').map((c, index) => (
+        <motion.span key={`split-text-${children}-${index}-${c}`} {...textType.char} {...charOptions}>
           {c}
         </motion.span>
       ))}
