0915e4eb198468ca46bd56899f9d035fbd6e6619
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-20 20:56:02 +0300

    add motion props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,24 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
+import { buttonMotion } from '../motion';
 import { rem } from '../utils';
 
 export const Button = ({ showOverlay, setShowOverlay }) => {
   return (
-    <Container className='button-container' onClick={() => void setShowOverlay((prev) => !prev)} $showOverlay={showOverlay}>
-      <h5>{!showOverlay ? 'MORE' : 'LESS'}</h5>
-      <img src='/icons/icon-arrow-up.svg' />
+    <Container
+      className='button-container'
+      onClick={() => void setShowOverlay((prev) => !prev)}
+      $showOverlay={showOverlay}
+      {...buttonMotion.container}
+    >
+      <motion.h5 {...buttonMotion.text}>{!showOverlay ? 'MORE' : 'LESS'}</motion.h5>
+      <motion.img src='/icons/icon-arrow-up.svg' alt='arrow' {...buttonMotion.image} />
     </Container>
   );
 };
 
-const Container = styled.button`
+const Container = styled(motion.button)`
   padding: ${rem(8)} ${rem(9)} ${rem(8)} ${rem(20)};
 
   display: flex;
