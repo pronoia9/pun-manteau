f04234027e498a873f590bb304b44ac6f0e52067
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-22 01:58:28 +0300

    add splittext + add animate()s + update styled comps

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,25 +1,39 @@
 import styled from 'styled-components';
-import { m } from 'framer-motion';
+import { m, animate } from 'framer-motion';
 
+import { SplitText } from '../components';
 import { buttonMotion } from '../motion';
 import { rem } from '../utils';
+import { useRef } from 'react';
 
 export const Button = ({ showOverlay, setShowOverlay }) => {
+  const imgRef = useRef();
+
+  const hovering = () => void animate(imgRef.current, { opacity: 0.5, scale: [1, 0.95, 1] }, { scale: { repeat: Infinity, duration: 1 } });
+
+  const whileHolding = () => void animate(imgRef.current, { opacity: 0.5, scale: 1.1 }, {});
+
+  const reset = () => void animate(imgRef.current, { opacity: 1, scale: 1 }, {});
+
   return (
     <Container
       className='button-container'
       onClick={() => void setShowOverlay((prev) => !prev)}
+      onMouseEnter={hovering}
+      onMouseLeave={reset}
+      onMouseDown={whileHolding}
+      onMouseUp={reset}
       $showOverlay={showOverlay}
       {...buttonMotion.container}
     >
-      <m.h5 {...buttonMotion.text}>{!showOverlay ? 'MORE' : 'LESS'}</m.h5>
-      <m.img src='/icons/icon-arrow-up.svg' alt='arrow' {...buttonMotion.image} />
+      <SplitText elementType='h5' text={!showOverlay ? 'MORE' : 'LESS'} />
+      <m.img ref={imgRef} src='/icons/icon-arrow-up.svg' alt='arrow' {...buttonMotion.image} />
     </Container>
   );
 };
 
 const Container = styled(m.button)`
-  padding: ${rem(8)} ${rem(9)} ${rem(8)} ${rem(20)};
+  padding: ${rem(8)} ${rem(9)} ${rem(8)} ${rem(9 + 13)};
 
   display: flex;
   flex-direction: row;
@@ -30,23 +44,18 @@ const Container = styled(m.button)`
   background: rgb(255, 255, 255);
   border-radius: ${rem(28)};
 
+  cursor: pointer;
+
   h5 {
     color: #000;
     font-size: 1rem;
     letter-spacing: ${rem(5)};
-    opacity: 0.5;
+    opacity: 0.5 !important;
   }
 
   img {
     width: ${rem(40)};
     height: ${rem(40)};
     border-radius: 50%;
-    transform: rotate(${({ $showOverlay }) => (!$showOverlay ? '180' : '0')}deg);
-  }
-
-  &:hover {
-    img {
-      opacity: 0.5;
-    }
   }
 `;
